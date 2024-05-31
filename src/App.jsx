25f9352d96eaaf4d87e9d39d257aacffb0decf94
Ryan Angala <rio@Ryans-MBP.vc.shawcable.net>
2024-05-31 10:55:43 -0700

    add modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.scss";
+import Modal from "./Modal/Modal";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Modal />
+  </div>;
 }
 
 export default App;
---
+++ src/Modal/Modal.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./Modal.scss";
+import exitIcon from "../assets/clear-svgrepo-com.svg";
+
+function Modal() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <button className="btn-modal" onClick={toggleModal}>
+        Open
+      </button>
+
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <div className="title-img__container">
+              <h2>TITLE</h2>
+              <img
+                className="close-modal"
+                onClick={toggleModal}
+                src={exitIcon}
+              />
+            </div>
+            <div className="content-container">
+            <div className="card-container">
+              <div className="img-container">
+                <img src="" alt="" />
+              </div>
+              <div className="card-content-container">
+                <h2 className="card-title">Netflix</h2>
+                <h4 className="card-subtitle">Streaming Service</h4>
+                <h3 className="card-price">$7.99</h3>
+              </div>
+            </div>
+            <div className="card-container">
+              <div className="img-container">
+                <img src="" alt="" />
+              </div>
+              <div className="card-content-container">
+                <h2 className="card-title">Sportsnet</h2>
+                <h4 className="card-subtitle">Sports</h4>
+                <h3 className="card-price">$2.99</h3>
+              </div>
+            </div>
+            <div className="card-container">
+              <div className="img-container">
+                <img src="" alt="" />
+              </div>
+              <div className="card-content-container">
+                <h2 className="card-title">CBC</h2>
+                <h4 className="card-subtitle">News Channel</h4>
+                <h3 className="card-price">$2.99</h3>
+              </div>
+            </div>
+            </div>
+            <div className="button-container">
+              <button className="checkout-button">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      </>
+  );
+}
+
+export default Modal;
