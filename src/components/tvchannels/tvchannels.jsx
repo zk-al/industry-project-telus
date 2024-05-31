6ad54b3fab3e69b16602820a4b294f1001b214ea
Grace Lau <grace@gracelau.ca>
2024-05-31 12:25:46 -0700

    Fixed styling to match Streaming Services + added functionality for adding items to calcualtor

--- src/components/tvchannels/tvchannels.jsx
+++ src/components/tvchannels/tvchannels.jsx
@@ -1,30 +1,58 @@
 import channels from "../../data/channels.json";
+import React, { useState } from 'react';
 import "../tvchannels/tvchannels.scss";
 
 
-function TvChannels () {
+const TvChannels = () => {
+    const [selectedChannel, setSelectedChannel] = useState([]);
+    const [ totalPrice, setTotalPrice] = useState(0);
+
+
+const handleSelectedChannel = (channel) => {
+    let updatedChannels;
+if (selectedChannel.includes(channel)){
+    updatedChannels = selectedChannel.filter(c =>channel.id !== channel.id);
+}else {
+    updatedChannels=[...selectedChannel,channel];
+}
+
+setSelectedChannel(updatedChannels);
+
+const total = updatedChannels.reduce((sum, c)=> sum + parseFloat(c.price.slice(1)),0);
+setTotalPrice (total.toFixed(2));
+};
+
+
     
  
 
     return (
-        <div className ="tvchannels__list-wrapper">
-            <h3 className ="tvchannels__list-heading"> </h3>
-            <ul className="tvchannels__list">
+        <div className ="tvchannels">
+            <h3 className ="tvchannels__title">TV Channels </h3>
+            <div className="tvchannels__container">
+                <div className= "tvchannels__list">
 
                 {channels.map((channel)=> (
-                <li 
+                <div
                 key={channel.id}
+                className={`tvchannels__card ${selectedChannel.includes(channel) ? 'selected' : ''}`}
+                onClick={() => handleSelectedChannel(channel)}>
+                     <div className="tvchannels__card-front">
             
-                data={channel}
-                >
-                {/* {channel.name} */}
-                <img src={channel.imageUrl} alt="thumbnail of video" className="tvchannels__list-img"/>
-                </li>
+            
+                <img src={channel.imageUrl} alt="{channel.name}" className="tvchannels__logo"/>
+                </div>
+
+                <div ClassName = "tvchannels__card-back">
+                <h3 className="tvchannels__provider">{channel.name}</h3>
+              <p className="tvchannels__price">{channel.price}</p>
+              </div>
+                </div>
                 ))}
             
-            </ul>
+            </div>
             
-
+        </div>
 
         </div>
     )
