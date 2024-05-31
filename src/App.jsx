8dbe6220f7d111183855f86ca13b53868b20128a
sayalibytes <sayalimehtayvr@gmail.com>
2024-05-31 18:08:33 +0530

    Add Streaming service providers component and connect with JSON data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.scss";
+import StreamingPartners from "./components/StreamingService/StreamingService";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <StreamingPartners />
+  </div>;
 }
 
 export default App;
