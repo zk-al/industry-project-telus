b12d9332d0889598adbfc7fffe491c58791b6e11
dlndn <65935030+dlndn@users.noreply.github.com>
2024-05-30 19:39:38 -0700

    added routes for homepage and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+        <Header />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" />
+                <Route path="/streaming" />
+                <Route path="/tv" />
+                <Route path="/theme-packs" />
+            </Routes>
+        </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,17 +7,22 @@ function Header() {
             <div className='logo'>
                 <img src='../../assets/images/Telus-Logo.svg' alt='Telus logo' className='logo__img' />
             </div>
+
             <nav className='navbar'>
-                <Link to={'/'} className='navbar__link navbar__link--streaming'>
+                <Link to={'/streaming'} className='navbar__link navbar__link--streaming'>
                     <p className='navbar__text'>Streaming</p>
                 </Link>
-                <Link to={'/'} className='navbar__link navbar__link--tv'>
+
+                <Link to={'/tv'} className='navbar__link navbar__link--tv'>
                     <p className='navbar__text'>TV</p>
                 </Link>
-                <Link to={'/'} className='navbar__link navbar__link--theme-packs'>
+
+                <Link to={'/theme-packs'} className='navbar__link navbar__link--theme-packs'>
                     <p className='navbar__text'>Theme Packs</p>
                 </Link>
             </nav>
         </header>
     );
 }
+
+export default Header;
