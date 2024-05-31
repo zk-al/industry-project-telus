bf869a35c662d34d352e6e05c43fcf1cfa34db11
Zack Lamarche <lamarche.zack@gmail.com>
2024-05-31 09:41:14 -0700

    Add working price calc function :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.scss";
+import PriceCalc from "./components/PriceCalc/PriceCalc";
+
+import footerImage from "./assets/images/telus-footer.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PriceCalc />
+      <img src={footerImage} alt="Image of the Telus Footer" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PriceCalc/PriceCalc.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import "./PriceCalc.scss";
+
+const demo = [
+  {
+    name: "ESPN",
+    category: "Sports",
+    price: 5.99,
+    image: "https://example.com/logos/espn.png",
+    selected: false,
+    id: 1,
+  },
+  {
+    name: "CNN",
+    category: "News",
+    price: 3.99,
+    image: "https://example.com/logos/cnn.png",
+    selected: false,
+    id: 2,
+  },
+  {
+    name: "HBO",
+    category: "Movies",
+    price: 10.99,
+    image: "https://example.com/logos/hbo.png",
+    selected: false,
+    id: 3,
+  },
+  {
+    name: "Discovery Channel",
+    category: "Documentary",
+    price: 4.99,
+    image: "https://example.com/logos/discovery.png",
+    selected: false,
+    id: 4,
+  },
+  {
+    name: "Cartoon Network",
+    category: "Kids",
+    price: 2.99,
+    image: "https://example.com/logos/cartoon_network.png",
+    selected: false,
+    id: 5,
+  },
+];
+
+function PriceCalc() {
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  // Getting clickedItem and put into selectedItems array when clicked
+  // When clicked again remove clickedItem from selectedItems array
+  const handleItemClick = (clickedItem) => {
+    setSelectedItems((prevSelectedItems) =>
+      prevSelectedItems.some((item) => item.id === clickedItem.id)
+        ? prevSelectedItems.filter((item) => item.id !== clickedItem.id)
+        : [...prevSelectedItems, clickedItem]
+    );
+  };
+
+  console.log(selectedItems);
+
+  const calculatedPrice = (selectedItems) => {
+    if (selectedItems !== " ") {
+      return selectedItems.reduce((sum, item) => sum + item.price, 0);
+    } else {
+      console.log("Arrayempty");
+    }
+  };
+
+  const totalPrice = calculatedPrice(selectedItems);
+  return (
+    <div>
+      {demo.map((channel) => (
+        <p
+          key={channel.id}
+          onClick={() => handleItemClick(channel)}
+          className={
+            selectedItems.some((selectedItem) => selectedItem.id === channel.id)
+              ? "highlight"
+              : ""
+          }
+        >
+          {channel.price}
+        </p>
+      ))}
+
+      <p>{totalPrice.toFixed(2)}</p>
+    </div>
+  );
+}
+
+export default PriceCalc;
